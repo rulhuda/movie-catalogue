6c9500f24896850dc5c20484aeb480671954ffeb
rulhuda <nurulhuda.unhasy@gmail.com>
2022-05-03 12:54:45 +0700

    Menginstall codecept dan puppeteer serta melakukan konfigurasi

---
+++ e2e/Liking_Movies.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+Feature('Liking Movies');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
